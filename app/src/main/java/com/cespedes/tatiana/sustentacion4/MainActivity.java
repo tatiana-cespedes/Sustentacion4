2bf4b5fef9295aa840b5cf4b9f07dca974ac434d
Tatiana Cespedes Torres <Tatiana Cespedes Torres>
2016-04-06 16:50:30 -0500

    Initial commit

---
+++ app/src/main/java/com/cespedes/tatiana/sustentacion4/MainActivity.java
@@ -0,0 +1,88 @@
+package com.cespedes.tatiana.sustentacion4;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(adapter);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+        };
+
+        ActionBar.Tab tab = actionBar.newTab().setTabListener(tabListener).setText("Chibugá");
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab().setTabListener(tabListener).setText("Túnel");
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab().setTabListener(tabListener).setText("Madre Laura");
+        actionBar.addTab(tab);
+
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                getSupportActionBar().setSelectedNavigationItem(position);
+            }
+        });
+
+
+
+    }
+
+    public class PagerAdapter extends FragmentPagerAdapter {
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case 0: return new Fra_Chibuga();
+                case 1: return new Fra_Tunel();
+                case 2: return new Fra_Laura();
+                default: return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/cespedes/tatiana/sustentacion4/Splash.java
@@ -0,0 +1,37 @@
+package com.cespedes.tatiana.sustentacion4;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent().setClass(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_DELAY);
+
+    }
+}
